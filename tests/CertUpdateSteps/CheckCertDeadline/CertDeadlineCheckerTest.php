38fe5b6988e058133081364bbd96cc8e2c54b6a4
Alexandr Yanul <ady@infoservice.ru>
2021-07-11 19:56:59 +0300

    CertDeadlineChecker и тестовые данные для него

---
+++ tests/CertUpdateSteps/CheckCertDeadline/CertDeadlineCheckerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CertUpdateSteps\CheckCertDeadline;
+
+use It5\CertUpdateSteps\CheckCertDeadline\CertDeadlineChecker;
+use It5\DebugLibs\DebugLib;
+use PHPUnit\Framework\TestCase;
+
+class CertDeadlineCheckerTest extends TestCase
+{
+    public function testCheck()
+    {
+        DebugLib::init('', DebugLib::MODE_WITH_OUTPUT);
+        $checker = new CertDeadlineChecker();
+        $certPath = __DIR__ . '/s-tsk.ru_wild-2029-12-02.pem';
+        $criticalRemainingDays = 7;
+        $result = $checker->check($certPath, $criticalRemainingDays);
+        $this->assertFalse($result);
+
+        $certPath = __DIR__ . '/notCert.txt';
+        $result = $checker->check($certPath, $criticalRemainingDays);
+        $this->assertFalse($result);
+
+        $certPath = __DIR__ . '/fullchain1-2020-06-12.pem';
+        $result = $checker->check($certPath, $criticalRemainingDays);
+        $this->assertTrue($result);
+    }
+}
