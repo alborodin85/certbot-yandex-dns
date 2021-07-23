a025664014909c7cd828653e312a0002d7e2d06b
Alexandr Yanul <ady@infoservice.ru>
2021-07-23 18:23:42 +0300

    CertbotYandexDnsTest дописан новыми шагами

--- tests/CertbotYandexDnsTest.php
+++ tests/CertbotYandexDnsTest.php
@@ -4,6 +4,9 @@ use It5\CertbotYandexDns;
 use PHPUnit\Framework\TestCase;
 use It5\CertUpdateSteps\CheckCertDeadline\CertDeadlineChecker;
 use It5\Adapters\YandexApi\YandexDnsApi;
+use It5\Adapters\CertbotDialog\CertbotDialog;
+use It5\Adapters\CertbotDialog\DialogDto;
+use It5\Env;
 
 class CertbotYandexDnsTest extends TestCase
 {
@@ -12,7 +15,7 @@ class CertbotYandexDnsTest extends TestCase
         $cliArgv = [__FILE__];
         $configAbsolutePath = __DIR__ . '/ParametersParser/DomainParametersStubs/domain-settings-stub.json';
         $logAbsolutePath = __DIR__ . 'test-app.log';
-        $object = CertbotYandexDns::singleton($cliArgv, $configAbsolutePath, $logAbsolutePath);
+        $object = CertbotYandexDns::singleton($cliArgv, $configAbsolutePath, $logAbsolutePath, Env::env()->yandexApiDelayMicroseconds);
 
         $checker = $this->getMockBuilder(CertDeadlineChecker::class)->getMock();
         $checker->expects($this->any())
@@ -20,14 +23,30 @@ class CertbotYandexDnsTest extends TestCase
             ->will($this->onConsecutiveCalls(true, false))
         ;
         $object->replaceDeadlineChecker($checker);
-        $result = $object->renewCerts();
 
-        $yandexDnsApi = $this->getMockBuilder(YandexDnsApi::class)->getMock();
+        $yandexDnsApi = $this->getMockBuilder(YandexDnsApi::class);
+        $yandexDnsApi = $this
+            ->getMockBuilder(YandexDnsApi::class)
+            ->setConstructorArgs([Env::env()->yandexApiDelayMicroseconds])
+            ->getMock();
+
         $yandexDnsApi->expects($this->any())
             ->method('delete')
             ->will($this->onConsecutiveCalls(0, 1));
         $object->replaceYandexDnsApi($yandexDnsApi);
 
+        $certbotDialog = $this->getMockBuilder(CertbotDialog::class)->getMock();
+        $certbotDialog->expects($this->any())
+            ->method('openDialog')
+            ->will($this->returnValue(new DialogDto()));
+        $certbotDialog->expects($this->any())
+            ->method('getRequiredDnsRecords')
+            ->will($this->returnValue([
+                ['parameter-name' => 'parameter-value']
+            ]));
+        $object->replaceCertbotDialog($certbotDialog);
+
+        $result = $object->renewCerts();
         $this->assertTrue($result);
     }
 }
